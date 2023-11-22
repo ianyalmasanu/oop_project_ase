150595d6bf36f5d822f6eafa8c09be27449b8c7e
IanyAlmasanu <almasanuiany10@stud.ase.ro>
2023-11-22 21:16:52 +0200

    working on movie teather class characteristics

--- Project1/MovieTeather.h
+++ Project1/MovieTeather.h
@@ -1,9 +1,25 @@
 #pragma once
+
 class MovieTeather
 {
 	char location;
-	int max_seats = 200;
-	int nRows = 20;
+	const int max_seats = 200;
+	const int nRows = 20;
+	const int seatsPerRow = 10;
+	int generated = 0;
+	int available_seats = max_seats - generated;
+	char currentMovies[10][30] = {
+		"Abracadabra",
+		"Plan B",
+		"Baltimore Dawn",
+		"Markus Zein",
+		"The Last Book",
+		"You were not here",
+		"Cars in space 4",
+		"The Message",
+		"1 Week",
+		"The Jam"
+	};
 
 };
 
