8a7266b63bad20c1a438291dbb145e905deb76ce
IanyAlmasanu <almasanuiany10@stud.ase.ro>
2023-11-22 20:37:24 +0200

    starting classes (design)

---
+++ Project1/MovieTeather.h
@@ -0,0 +1,9 @@
+#pragma once
+class MovieTeather
+{
+	char location;
+	int max_seats = 200;
+	int nRows = 20;
+
+};
+
--- Project1/source.cpp
+++ Project1/source.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <conio.h>
+#include <stdlib.h>
 
 int main() {
 std::cout << std::endl << "Hello World!";
