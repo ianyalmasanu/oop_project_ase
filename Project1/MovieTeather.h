2ba08f7166a282ae95c36ae421b00c05dbc154ee
IanyAlmasanu <almasanuiany10@stud.ase.ro>
2023-11-22 22:49:29 +0200

    added seat number generator (method)

--- Project1/MovieTeather.h
+++ Project1/MovieTeather.h
@@ -2,12 +2,13 @@
 
 class MovieTeather
 {
-	char location;
+private:
 	const int max_seats = 200;
-	const int nRows = 20;
-	const int seatsPerRow = 10;
+	const int nRows = 10;
+	const int seatsPerRow = 20;
 	int generated = 0;
 	int available_seats = max_seats - generated;
+	char location;
 	char currentMovies[10][30] = {
 		"Abracadabra",
 		"Plan B",
@@ -20,6 +21,16 @@ class MovieTeather
 		"1 Week",
 		"The Jam"
 	};
+
+
+	void GenerateSeatNo() {
+	
+		for (int row = 1; row <= nRows; row++) {
+			for (int seat = 1; seat <= seatsPerRow; seat++) {
+				int seatNumber = (row - 1) * seatsPerRow + seat; //e.g. max seat number is 200 and max row is 10
+			}
+		}
+	}
 
 };
 
